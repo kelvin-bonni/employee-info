de036437d28df4bdf4263a14ec2389aaf1fbe4e8
Kelvin Bonni <kelvin.bonni@trilogy.com>
2023-05-05 01:40:39 +0200

    Refactoring

--- src/main/java/com/challenge/dcscodechallenge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/challenge/dcscodechallenge/exception/GlobalExceptionHandler.java
@@ -1,38 +1,21 @@
 package com.challenge.dcscodechallenge.exception;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = DuplicateBookException.class)
-    public ModelAndView duplicateBookException(DuplicateBookException e) {
-        final ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("ref", e.getEmployee().getId());
-        modelAndView.addObject("object", e.getEmployee());
-        modelAndView.addObject("message", "Cannot add an already existing book");
-        modelAndView.setViewName("error-employee");
-        return modelAndView;
-    }
-
     //handles MethodArgumentNotValidException
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ModelAndView handleMethodArgumentNotValidException(
